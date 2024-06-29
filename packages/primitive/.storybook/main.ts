966d906dbb0004c8a996b3058387b829d77f60c0
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-29 14:37:04 +0900

    chore: storybook 설정 파일 typescript 기반으로 수정

--- packages/primitive/.storybook/main.ts
+++ packages/primitive/.storybook/main.ts
@@ -4,11 +4,10 @@ import { join, dirname } from "path";
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value) {
+function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 
-/** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
   stories: [
     "../components/**/*.mdx",
