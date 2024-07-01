2e8e8952fad43cab7fadd816e318a4fb4f216a00
ShinYoung-Kim <lukey0515@naver.com>
2024-07-01 13:33:25 +0900

    style: manifest 파일 분리

---
+++ packages/plugins/boilerplate/figma.manifest.ts
@@ -0,0 +1,14 @@
+export default {
+	name: "warrr",
+	id: "1387759100113360584",
+	api: "1.0.0",
+	main: "code.js",
+	capabilities: [],
+	enableProposedApi: false,
+	documentAccess: "dynamic-page",
+	editorType: ["figma"],
+	ui: "index.html",
+	networkAccess: {
+		allowedDomains: ["none"],
+	},
+};
