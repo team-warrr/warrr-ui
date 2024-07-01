a151bc82d30ad0bc654c67d418b90b95e0602514
ShinYoung-Kim <lukey0515@naver.com>
2024-07-01 13:33:00 +0900

    style: 불필요한 코드 제거

--- packages/plugins/boilerplate/src/plugin/code.ts
+++ packages/plugins/boilerplate/src/plugin/code.ts
@@ -1,18 +1,1 @@
 figma.showUI(__html__);
-
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
-
-  figma.closePlugin();
-};
