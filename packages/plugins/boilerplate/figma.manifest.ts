0493516be7610e5033a41006f75484637f970912
ShinYoung-Kim <lukey0515@naver.com>
2024-07-02 00:16:00 +0900

    style: manifest에서 불필요한 속성 제거

--- packages/plugins/boilerplate/figma.manifest.ts
+++ packages/plugins/boilerplate/figma.manifest.ts
@@ -1,14 +1,7 @@
 export default {
 	name: "warrr",
-	id: "1387759100113360584",
 	api: "1.0.0",
 	main: "code.js",
-	capabilities: [],
-	enableProposedApi: false,
-	documentAccess: "dynamic-page",
-	editorType: ["figma"],
 	ui: "index.html",
-	networkAccess: {
-		allowedDomains: ["none"],
-	},
+	editorType: ["figma"],
 };
