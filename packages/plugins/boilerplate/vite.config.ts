bccd636d9022473aaac811d7eed6707330519a6e
ShinYoung-Kim <lukey0515@naver.com>
2024-07-01 13:33:00 +0900

    feat: vite 빌드 시 파일 이름 난독화 제거

--- packages/plugins/boilerplate/vite.config.ts
+++ packages/plugins/boilerplate/vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		rollupOptions: {
+			input: [path.resolve("src/plugin/code.ts"), path.resolve("index.html")],
+			output: {
+				entryFileNames: "[name].js",
+			},
+		},
+		outDir: path.resolve("dist"),
+	},
 });
