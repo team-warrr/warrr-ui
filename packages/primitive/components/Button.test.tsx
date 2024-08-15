c3da7cee1a12fcd056882e5a81ab96dcb5b94b95
mj <minai621@naver.com>
2024-08-15 14:11:45 +0900

    chore: Button 테스트 description 한글로 번역

--- packages/primitive/components/Button.test.tsx
+++ packages/primitive/components/Button.test.tsx
@@ -2,8 +2,8 @@ import { render } from "@testing-library/react";
 
 import { Button } from "./Button";
 
-describe("Button", () => {
-  it("should render correctly", () => {
+describe("버튼 컴포넌트 테스트", () => {
+  it("렌더링이 올바르게 되는지 확인합니다.", () => {
     const wrapper = render(<Button label="Button" />);
 
     expect(() => wrapper.unmount()).not.toThrow();
