b20d9d382e22f380d9bc67ad981f8a490972621c
mj <minai621@naver.com>
2024-08-16 06:07:36 +0900

    chore: button에 불필요한 텍스트 삭제

--- packages/primitive/components/Button.tsx
+++ packages/primitive/components/Button.tsx
@@ -40,7 +40,7 @@ export const Button = ({
       className={["storybook-button", `storybook-button--${size}`, mode].join(" ")}
       {...props}
     >
-      {label}x
+      {label}
       <style>{`
         button {
           background-color: ${backgroundColor};
