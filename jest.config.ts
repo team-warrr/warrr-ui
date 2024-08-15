dde171f8764cc70ed4d51e5acc6535fcf0c4a853
mj <minai621@naver.com>
2024-08-15 14:03:33 +0900

    chore: jest 설정 코멘트 삭제

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,7 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  // The test environment that will be used for testing
   testEnvironment: "jsdom",
-  // A preset that is used as a base for Jest's configuration
   preset: "ts-jest",
 };
 
