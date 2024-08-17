3a192bc07661ab8f4641f9d2d7b37d8d791fa7c4
mj <minai621@naver.com>
2024-08-17 21:27:09 +0900

    chore: button 변경

--- packages/primitive/components/Button.tsx
+++ packages/primitive/components/Button.tsx
@@ -41,7 +41,7 @@ export const Button = ({
       className={["storybook-button", `storybook-button--${size}`, mode].join(" ")}
       {...props}
     >
-      {label}
+      {label}x
       <style>{`
         button {
           background-color: ${backgroundColor};
