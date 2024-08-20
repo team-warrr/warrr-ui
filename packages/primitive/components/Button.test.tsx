ddc3481ac92e81091e80f88bc3f19d0190ebb65b
MJ <80272444+minai621@users.noreply.github.com>
2024-08-20 10:18:45 +0900

    chore/#37 jest 설정 (#43)
    * chore: jest 설치
    
    preset으로 ts-jest 적용
    
    * chore: button에 대한 예시 테스트 코드 추가
    
    * chore: ts-node 의존성 설치
    
    * chore: jest 설정 파일에 주석 제거
    
    * chore: jest 설정 코멘트 삭제
    
    * chore: Button 테스트 description 한글로 번역
    
    * chore: 의존성 업데이트
    
    * chore: commit 시 jest 테스트 실행

---
+++ packages/primitive/components/Button.test.tsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+
+import { Button } from "./Button";
+
+describe("버튼 컴포넌트 테스트", () => {
+  it("렌더링이 올바르게 되는지 확인합니다.", () => {
+    const wrapper = render(<Button label="Button" />);
+
+    expect(() => wrapper.unmount()).not.toThrow();
+  });
+});
