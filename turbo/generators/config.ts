db8d5bc4751210145f6db261dd7ac37cc0351c07
홍서현 <ghdtjgus76@naver.com>
2024-08-20 18:58:28 +0900

    feat/#12 Primitive UI codegen 세팅 (#42)
    * feat: 반영되지 않은 패키지 변경사항 반영
    
    * chore: 불필요한 gitkeep 파일 삭제
    
    * feat: 컴포넌트 스토리 템플릿 추가
    
    * feat: 컴포넌트 테스트 템플릿 추가
    
    * feat: 컴포넌트 템플릿 추가
    
    * feat: package.json 템플릿 추가
    
    * feat: 리드미 템플릿 추가
    
    * feat: tsconfig.json 템플릿 추가
    
    * feat: tsup.config.ts 템플릿 추가
    
    * rename: 테스트, 컴포넌트, 스토리 템플릿 폴더 구조 세팅
    
    * feat: root devDependency에 tsup 설치
    
    * docs: tsup config 파일 템플릿 추가
    
    * docs: tsconfig 파일 템플릿 추가
    
    * docs: 리드미 파일 템플릿 추가
    
    * docs: 스토리 파일 템플릿 추가
    
    * feat: root devDependency에 @testing-library/react 설치
    
    * docs: 테스트 파일 템플릿 추가
    
    * docs: 컴포넌트 파일 템플릿 추가
    
    * docs: 컴포넌트 배럴 파일 템플릿 추가
    
    * docs: 컴포넌트 package.json 템플릿 추가
    
    * feat: root devDependency에 @turbo/gen 패키지 설치
    
    * chore: 불필요한 파일 삭제
    
    * feat: primitive 컴포넌트 base file generator 작동하도록 turbo gen 설정
    
    * feat: primitive, themed 패키지 중 생성할 패키지 고를 수 있도록 기능 추가
    
    * fix: tsconfig.json 템플릿 루트 tsconfig.json 경로 잘못 지정한 부분 수정
    
    * fix: tsup.config.ts 템플릿 target es2020으로 수정
    
    * chore: 테스트 파일 템플릿 수정
    
    * chore: tsconfig.json 파일 템플릿 수정
    
    * chore: 컴포넌트 파일 템플릿 수정
    
    * chore: 컴포넌트 배럴 파일 템플릿 수정
    
    * chore: 컴포넌트 스토리 파일 템플릿 수정
    
    * chore: root tsconfig 설정 변경
    
    * chore: 자잘한 오타 반영
    
    * chore: package.json build 스크립트 수정
    
    * chore: package.json peerDependency로 next 포함하도록 수정
    
    * chore: 스토리북 예시 파일 복원
    
    * chore: turbo gen prompts message 한글로 변경
    
    * chore: component 템플릿 타입 추론되도록 수정
    
    * chore: component package.json 템플릿 peerDependency에서 next 삭제
    
    * chore: turbo gen generator name, description message 한글로 변경
    
    * chore: package.json 템플릿 name primitive, themed 구분하도록 수정
    
    * chore: package.json 템플릿 name 포맷 변경
    
    * feat: 테스트 코드 템플릿 테스트 케이스 추가
    
    * chore: props 타입 컨벤션 변경에 따른 변경사항 적용
    
    * chore: prettierignore 목록에 turbo 폴더 추가
    
    * feat: pnpm lock 파일 변경사항 반영
    
    * feat: root format 스크립트 추가
    
    * fix: 사라진 prettier 설정 수정
    
    * feat: plop format 기능 추가
    
    * feat: plop kebabCase helper 추가
    
    * chore: plop 생성 파일명 kebabCase로 변경
    
    * feat: root에 turbo gen 스크립트 추가
    
    * feat: prettierignore에 pnpm-lock 파일 추가
    
    * chore: eol 이슈 해결

---
+++ turbo/generators/config.ts
@@ -0,0 +1,64 @@
+import { exec } from "child_process";
+import { promisify } from "util";
+
+import type { PlopTypes } from "@turbo/gen";
+
+function toKebabCase(input: string): string {
+  return input
+    .replace(/([a-z])([A-Z])/g, '$1-$2') 
+    .replace(/\s+/g, '-')                
+    .toLowerCase();                     
+}
+
+
+
+export default function generator(plop: PlopTypes.NodePlopAPI): void {
+  plop.setHelper("kebabCase", (text: string) => {
+    return toKebabCase(text);
+  });
+
+  plop.setActionType("format", async () => {
+    try {
+      const execPromise = promisify(exec);
+      const { stdout, stderr } = await execPromise("pnpm format");
+  
+      if (stderr) {
+        throw new Error(`pnpm format 실패: ${stderr}`);
+      }
+  
+      console.log(stdout);
+      return "pnpm format이 성공적으로 적용되었습니다.";
+    } catch (error) {
+      throw new Error(`pnpm format 실패: ${(error as Error).message}`);
+    }
+  });
+
+  plop.setGenerator("컴포넌트 기본 파일 생성기", {
+    description: "컴포넌트의 기본 파일을 생성합니다.",
+    prompts: [
+      {
+        type: "input",
+        name: "componentName",
+        message: "생성할 컴포넌트의 이름을 입력하세요 (예: Button): ",
+      },
+      {
+        type: "list",
+        name: "packageName",
+        message: "컴포넌트를 생성할 패키지를 선택하세요: \n",
+        choices: ["primitive", "themed"],
+      },
+    ],
+    actions: [
+      {
+        type: "addMany",
+        templateFiles: "./templates/component/**",
+        base: "./templates/component",
+        destination: `packages/{{packageName}}/components/{{kebabCase componentName}}`,
+        abortOnFail: true,
+      },
+      {
+        type: "format",
+      },
+    ],
+  });
+}
