eeaf024eaf5acb867e5fee74dabce6d7f71aff6f
gs0428 <lider28@naver.com>
2024-08-28 02:16:16 +0900

    chore: prettier 적용

--- packages/primitive/components/divider/__tests__/divider.test.tsx
+++ packages/primitive/components/divider/__tests__/divider.test.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { createRef } from "react";
 
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 
 import { Divider } from "../src";
