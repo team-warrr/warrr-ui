c2592ea391ec55edad74d9217640118137f039f1
gs0428 <lider28@naver.com>
2024-08-28 02:18:50 +0900

    feat: Divider 컴포넌트 구현

--- packages/primitive/components/divider/src/divider.tsx
+++ packages/primitive/components/divider/src/divider.tsx
@@ -1,11 +1,19 @@
 import type { ComponentProps, ForwardedRef } from "react";
 import { forwardRef } from "react";
 
-export interface DividerProps extends ComponentProps<"div"> {}
+const DEFAULT_ORIENTATION = "horizontal";
+
+type Orientation = "horizontal" | "vertical";
+export interface DividerProps extends ComponentProps<"div"> {
+  orientation?: Orientation;
+}
 
 const Divider = forwardRef<HTMLDivElement, DividerProps>(
   ({ ...rest }, ref: ForwardedRef<HTMLDivElement>) => {
-    return <div ref={ref} {...rest}></div>;
+    const { orientation: orientaionProp = DEFAULT_ORIENTATION, ...restProps } = rest;
+    const orientaion = orientaionProp === "vertical" ? "vertical" : DEFAULT_ORIENTATION;
+
+    return <div role="separator" aria-orientation={orientaion} ref={ref} {...restProps} />;
   }
 );
 
