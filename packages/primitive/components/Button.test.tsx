cc099d3817c03efae204542f763dbed934c169b3
MJ <80272444+minai621@users.noreply.github.com>
2024-09-01 20:42:29 +0900

    chore/#65 jest.setup.ts 설정 (#72)
    * chore: jest.setup.ts와 관련 의존성 설정
    
    * feat: button rtl 테스트 추가
    
    * chore: @testing-library/jest-dom를 devDependencies로 수정

--- packages/primitive/components/Button.test.tsx
+++ packages/primitive/components/Button.test.tsx
@@ -8,4 +8,10 @@ describe("버튼 컴포넌트 테스트", () => {
 
     expect(() => wrapper.unmount()).not.toThrow();
   });
+
+  it("rtl을 사용해 렌더링이 올바르게 되는지 확인합니다.", () => {
+    const wrapper = render(<Button label="Button" />);
+
+    expect(wrapper.getByText("Button")).toBeInTheDocument();
+  });
 });
