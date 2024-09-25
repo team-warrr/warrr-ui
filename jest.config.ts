ec07006258d7ab67a5fa97f70c9efbd7fe143c98
ShinYoung-Kim <83866983+ShinYoung-Kim@users.noreply.github.com>
2024-09-25 16:06:17 +0900

    chore/#93 husky 오류 해결 (#112)
    * fix: pre-commit 오류 일으키는 코드 제거
    
    * fix: jest testMatch 수정
    
    * refactor: husky 구버전 관련 코드 pre push에서 제거

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "jest";
 const config: Config = {
   testEnvironment: "jsdom",
   preset: "ts-jest",
-  testMatch: ["<rootDir>/packages/**/?(*.)+(spec|test).[jt]s?(x)"],
+  testMatch: ["<rootDir>/packages/**/__tests__/**/*.(spec|test).[jt]s?(x)"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
 
