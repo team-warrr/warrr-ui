a9f180724113cb6aba02dbe31d78e0a174055a7e
ShinYoung-Kim <lukey0515@naver.com>
2024-10-07 15:41:26 +0900

    feat: 로딩 컴포넌트 구현

--- packages/primitive/components/loading/src/loading.tsx
+++ packages/primitive/components/loading/src/loading.tsx
@@ -1,11 +1,21 @@
-import type { ComponentProps, ForwardedRef } from "react";
+import type { ElementType, ForwardedRef, ReactNode } from "react";
 import { forwardRef } from "react";
 
-export interface LoadingProps extends ComponentProps<"div"> {}
+export interface LoadingProps<E extends ElementType = "div"> {
+  as?: E;
+  children?: ReactNode;
+}
 
 const Loading = forwardRef<HTMLDivElement, LoadingProps>(
   (props, ref: ForwardedRef<HTMLDivElement>) => {
-    return <div ref={ref} {...props}></div>;
+    const defaultFallback = "로딩중입니다...";
+    const { as: Component = "div", children = defaultFallback, ...restProps } = props;
+
+    return (
+      <Component ref={ref} {...restProps} role="status" aria-live="polite" aria-busy="true">
+        {children}
+      </Component>
+    );
   }
 );
 
