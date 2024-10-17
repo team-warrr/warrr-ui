5dff0aa7bdb63ecd8e27420298df362312ff4e12
ShinYoung-Kim <lukey0515@naver.com>
2024-10-17 14:22:08 +0900

    fix: jest 타입 오류 수정

--- packages/primitive/components/loading/__tests__/loading.test.tsx
+++ packages/primitive/components/loading/__tests__/loading.test.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { createRef } from "react";
 
+import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 
 import { Loading } from "../src";
