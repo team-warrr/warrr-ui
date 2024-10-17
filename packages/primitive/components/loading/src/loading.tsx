809fb105972e9a4d5ed096292232cd967cdbbafa
ShinYoung-Kim <lukey0515@naver.com>
2024-10-17 14:22:07 +0900

    fix: 다형성 고려하여 타입 수정

--- packages/primitive/components/loading/src/loading.tsx
+++ packages/primitive/components/loading/src/loading.tsx
@@ -1,13 +1,33 @@
-import type { ElementType, ForwardedRef, ReactNode } from "react";
+import type { ElementType, ReactNode } from "react";
 import { forwardRef } from "react";
 
-export interface LoadingProps<E extends ElementType = "div"> {
+type AsProp<T extends ElementType> = {
+  as?: T;
+};
+
+type PolymorphicRef<T extends ElementType> = React.ComponentPropsWithRef<T>["ref"];
+
+type PolymorphicComponentProps<
+  T extends React.ElementType,
+  Props = Record<string, never>,
+> = AsProp<T> &
+  React.ComponentPropsWithoutRef<T> &
+  Props & {
+    ref?: PolymorphicRef<T>;
+  };
+
+interface _LoadingProps<E extends ElementType> {
   as?: E;
   children?: ReactNode;
 }
 
-const Loading = forwardRef<HTMLDivElement, LoadingProps>(
-  (props, ref: ForwardedRef<HTMLDivElement>) => {
+export type LoadingProps<E extends ElementType = "div"> = PolymorphicComponentProps<
+  E,
+  _LoadingProps<E>
+>;
+
+const Loading = forwardRef(
+  <E extends ElementType = "div">(props: LoadingProps<E>, ref: PolymorphicRef<E>["ref"]) => {
     const defaultFallback = "로딩중입니다...";
     const { as: Component = "div", children = defaultFallback, ...restProps } = props;
 
